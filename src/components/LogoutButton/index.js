639c3017bfdd55052539cf88a04e6b9d4c145cc8
chandra-mouli1652 <chandubynapalepu21@gmail.com>
2023-09-03 11:21:41 +0000

    authentication2 solution

---
+++ src/components/LogoutButton/index.js
@@ -0,0 +1,22 @@
+// Write your JS code here
+import './index.css'
+import Cookies from 'js-cookie'
+import {withRouter} from 'react-router-dom'
+
+const LogoutButton = props => {
+  const onClickLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <div className="Logout-container">
+      <button className="Logout-button" type="button" onClick={onClickLogout}>
+        Logout
+      </button>
+    </div>
+  )
+}
+
+export default withRouter(LogoutButton)
